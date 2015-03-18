29b59d2dc4e95e5b83c9351af0cadc163aae51e2
Kate <katebeavis84@gmail.com>
2015-03-18 23:00:43 +0000

    Added auto search

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,17 +1,10 @@
-githubUserSearch.controller('GitUserSearchController', function($scope) {
+githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
-  $scope.doSearch = function() {
-    $scope.searchResult = {
-      "items":  [{
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }]
-    };
+  var searchResource = $resource("https://api.github.com/search/users");
+
+    $scope.doSearch = function() {
+      $scope.searchResult = searchResource.get({
+        q: $scope.searchTerm
+    });
   };
 });
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -8,7 +8,7 @@ describe ('GitUserSearchController', function() {
     ctrl = $controller('GitUserSearchController', {
       $scope: scope
     });
-}));
+  }));
 
     it("initialises with an empty search result and term", function() {
       expect(scope.searchResult).toBeUndefined();
@@ -16,6 +16,16 @@ describe ('GitUserSearchController', function() {
     });
 
     describe("when searching for a user", function() {
+
+      var httpBackend;
+      beforeEach(inject(function($httpBackend) {
+        httpBackend = $httpBackend
+        httpBackend
+          .when("GET", "https://api.github.com/search/users?q=hello")
+          .respond(
+            { items: items }
+          );
+      }));
 
       var items = [{
         "login": "tansaku",
@@ -28,8 +38,10 @@ describe ('GitUserSearchController', function() {
       }];
 
     it("displays search results", function() {
-      scope.searchResult = "Hello";
+      scope.searchTerm = "hello";
       scope.doSearch();
+      scope.$apply();
+      httpBackend.flush();
       expect(scope.searchResult.items).toEqual(items);
     });
   });
